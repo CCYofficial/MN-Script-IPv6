fc61e9baa89bb20d6d632b8ab809221e72fb81d7
Team CCY <54861297+CCYofficial@users.noreply.github.com>
2019-10-14 10:23:49 +0200

    initial

--- ccy_IPv6.sh
+++ ccy_IPv6.sh
@@ -249,7 +249,7 @@ fi
 # Start cryptocurrency daemon, wait for wallet creation //
 sudo systemctl start ccycore &&
 echo "" ; echo "Please wait for few minutes..."
-sleep 120 &
+sleep 30 &
 PID=$!
 i=1
 sp="/-\|"
@@ -282,7 +282,7 @@ fi
 echo ""
 echo "Give some time to shutdown the wallet..."
 echo ""
-sleep 60 &
+sleep 30 &
 PID=$!
 i=1
 sp="/-\|"
